e9750fd5bd1ea5a0c7095f3223c8dd979222e3e1
Kashif Mubarak <mubarak.kashif@gmail.com>
2020-10-19 14:17:17 -0500

    Fixed entity model to point to local sql server database in MVC project.

--- Playground.Mvc/DAL/SeraphDbContext.Designer.cs
+++ Playground.Mvc/DAL/SeraphDbContext.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\km031369\Desktop\Playground\Playground.Mvc\DAL\SeraphDbContext.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\kashi\Source\Repos\Playground\Playground.Mvc\DAL\SeraphDBContext.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
