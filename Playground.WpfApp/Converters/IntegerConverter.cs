56c403291f4e3e363668af109db7ab6fe37f81ca
Kashif Mubarak <mubarak.kashif@gmail.com>
2021-09-02 14:45:08 -0500

    Added Integer converter for inline editing UI in WPF Project.

---
+++ Playground.WpfApp/Converters/IntegerConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Playground.WpfApp.Converters
+{
+    /// <summary>
+    /// Converts the value to 0 if its null 
+    /// </summary>
+    public class IntegerConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+
+            {
+                string strVal = value.ToString();
+
+                if (string.IsNullOrEmpty(strVal))
+                {
+                    return 0;
+                }
+                else
+                {
+                    return int.Parse(strVal);
+                }
+            }
+
+            catch (Exception)
+            {
+                return 0;
+            }
+        }
+    }
+}
